59d38560570a8cc96f84189a530c24046039ef4f
Kang Yu <yukangmbbz@163.com>
2016-09-26 00:14:41 +0800

    Update computational_physics homework 3-L1.py

--- Exercise-3/computational_physics homework 3-L1.py
+++ Exercise-3/computational_physics homework 3-L1.py
@@ -17,16 +17,16 @@ h = ("\n")
 j = (" ")
 i = 1
 while i < 20:
-    print(a+h+b+h+c+h+d+h+e+h+f+h+g+h)
+    print(a + h + b + h + c + h + d + h + e + h + f + h + g + h)
     time.sleep(0.5)
-    i = i+1
-    a = j+a
-    b = j+b
-    c = j+c
-    d = j+d
-    e = j+e
-    f = j+f
-    g = j+g
+    i = i + 1
+    a = j + a
+    b = j + b
+    c = j + c
+    d = j + d
+    e = j + e
+    f = j + f
+    g = j + g
     os.system('cls')
     
 input("please Enter:")
