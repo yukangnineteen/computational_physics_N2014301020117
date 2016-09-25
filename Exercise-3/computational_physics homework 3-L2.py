831136de7d927239577d9e4c80ce7ca2f96705b2
Kang Yu <yukangmbbz@163.com>
2016-09-26 00:17:24 +0800

    Update computational_physics homework 3-L2.py

--- Exercise-3/computational_physics homework 3-L2.py
+++ Exercise-3/computational_physics homework 3-L2.py
@@ -12,12 +12,12 @@ c=("◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆\n")
 d=("◆◆\n")
 e=("                ◆◆◆◆\n")
 f=("                              ◆◆\n")
-li=[15*a+2*b,4*c+8*d,2*b+15*e,8*f+4*c]
+li=[15*a+2*b, 4*c+8*d, 2*b+15*e, 8*f+4*c]
 i=0
-while i<20:
+while i < 20:
     print(li[i%4])
     time.sleep(1)
-    i=i+1
+    i = i+1
     os.system('cls')
     
 input("please Enter:")
